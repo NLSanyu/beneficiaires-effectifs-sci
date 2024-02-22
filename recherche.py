fbbae54a3e5c9885187f3fe0296508917a6352ea
Lydia Sanyu Naggayi <37118361+NLSanyu@users.noreply.github.com>
2024-02-22 19:42:40 +0100

    Update search feature

--- recherche.py
+++ recherche.py
@@ -1,19 +1,31 @@
+"""
+Recherche par nom ou par entreprise
+"""
+
 from api_pappers import fetch_data_beneficiaires_effectifs, fetch_data_entreprises
 
 
 def recherche_par_nom(nom=None):
     """Recherche par nom du bénéficiaire effectif"""
     data_nom = fetch_data_beneficiaires_effectifs(nom)
-    benef_list = []
+    beneficiaires_list = []
 
     for item_n in data_nom["resultats"][1]["entreprises"]:
         siren = item_n["siren"]
         nom_entreprise = item_n["nom_entreprise"]
         data_entreprise = fetch_data_entreprises(siren)
         for item_e in data_entreprise["beneficiaires_effectifs"]:
-            benef_list.append({nom_entreprise: f"{item_e['nom']} {item_e['prenom']}"})
+            beneficiaires_list.append({nom_entreprise: f"{item_e['nom']} {item_e['prenom']}"})
 
-    return f"Nom : {nom}, Entreprises : {benef_list}"
+    return f"Nom : {nom}, Entreprises : {beneficiaires_list}"
 
 def recherche_par_entreprise(siren=None):
     """Recherche par siren entreprise"""
+    beneficiaires_list = []
+    data_entreprise = fetch_data_entreprises(siren)
+    nom_entreprise = data_entreprise["nom_entreprise"]
+
+    for item in data_entreprise["beneficiaires_effectifs"]:
+        beneficiaires_list.append({nom_entreprise: f"{item['nom']} {item['prenom']}"})
+
+    return f"Entreprise : {nom_entreprise}, Beneficiaires : {beneficiaires_list}"
